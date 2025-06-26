06730c0fd4cc3f335d2d7c180adfa9038c4f9df3
Ali Rayyan <alirayyan1717@gmail.com>
2025-06-26 19:21:17 +0500

    It will scroll to top upon clicking on start

--- script.js
+++ script.js
@@ -7,6 +7,60 @@ let totalBreakSeconds;
 let originalBreakTime;
 let isBreak = false;
 
+
+function startTimer() {
+    clearInterval(timer);
+    isBreak = false;
+    // let minutes = document.getElementById("minutesInput").value; this is used to get the input from index.html but this is saved as string so to convert into integer we parse it. 
+    document.getElementById("pomodoroSound").play();
+    let minutes = parseInt(document.getElementById("WorkminutesInput").value);
+    let seconds = parseInt(document.getElementById("WorksecondsInput").value);
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+    if (minutes < 0 || seconds < 0) {
+        alert("Please enter non-negative values.");
+        return;
+    }
+    if (isNaN(minutes)) minutes = 0;
+    if (isNaN(seconds)) seconds = 0;
+    // isNan Means not a number 
+    totalSeconds = seconds + (minutes * 60);
+    originalTime = totalSeconds
+    if (totalSeconds <= 0) {
+        alert("Please enter a valid Pomodoro duration.");
+        return;
+    }
+
+    timer = setInterval(() => {
+        if (totalSeconds <= 0) {
+            clearInterval(timer)
+            sessionCount++;
+            document.getElementById("timerDisplay").textContent = "break starting"
+            document.getElementById("sessionCount").textContent = `Sessions: ${sessionCount}`
+            startBreakTimer()
+        }
+        minutes = Math.floor(totalSeconds / 60)
+        seconds = totalSeconds % 60
+        let paddedminutes = String(minutes).padStart(2, '0');
+        let paddedseconds = String(seconds).padStart(2, '0');
+        document.getElementById("timerDisplay").textContent = `${paddedminutes}:${paddedseconds}`
+        totalSeconds = totalSeconds - 1
+        let progress = totalSeconds / originalTime;
+        let angle = 360 * (1 - progress);
+        document.querySelector('.ring').style.background = `conic-gradient(#333 ${angle}deg, orange ${angle}deg)`;
+
+
+
+    }, 1000)
+
+    // document.getElementById("WorkminutesInput").value = "";
+    // document.getElementById("WorksecondsInput").value = "";
+}
+
 function startBreakTimer() {
     clearInterval(timer)
     isBreak = true;
@@ -56,57 +110,6 @@ function startBreakTimer() {
     }, 1000)
     // document.getElementById("BreakminutesInput").value = "";
     // document.getElementById("BreaksecondsInput").value = "";
-
-}
-
-function startTimer() {
-    clearInterval(timer);
-    isBreak = false;
-    // let minutes = document.getElementById("minutesInput").value; this is used to get the input from index.html but this is saved as string so to convert into integer we parse it. 
-    document.getElementById("pomodoroSound").play();
-    let minutes = parseInt(document.getElementById("WorkminutesInput").value);
-    let seconds = parseInt(document.getElementById("WorksecondsInput").value);
-
-
-    if (minutes < 0 || seconds < 0) {
-        alert("Please enter non-negative values.");
-        return;
-    }
-    if (isNaN(minutes)) minutes = 0;
-    if (isNaN(seconds)) seconds = 0;
-    // isNan Means not a number 
-    totalSeconds = seconds + (minutes * 60);
-    originalTime = totalSeconds
-    if (totalSeconds <= 0) {
-        alert("Please enter a valid Pomodoro duration.");
-        return;
-    }
-    
-    timer = setInterval(() => {
-        if (totalSeconds <= 0) {
-            clearInterval(timer)
-            sessionCount++;
-            document.getElementById("timerDisplay").textContent = "break starting"
-            document.getElementById("sessionCount").textContent = `Sessions: ${sessionCount}`
-            startBreakTimer()
-        }
-        minutes = Math.floor(totalSeconds / 60)
-        seconds = totalSeconds % 60
-        let paddedminutes = String(minutes).padStart(2, '0');
-        let paddedseconds = String(seconds).padStart(2, '0');
-        document.getElementById("timerDisplay").textContent = `${paddedminutes}:${paddedseconds}`
-        totalSeconds = totalSeconds - 1
-        let progress = totalSeconds / originalTime;
-        let angle = 360 * (1 - progress);
-        document.querySelector('.ring').style.background = `conic-gradient(#333 ${angle}deg, orange ${angle}deg)`;
-
-
-
-    }, 1000)
-
-    // document.getElementById("WorkminutesInput").value = "";
-    // document.getElementById("WorksecondsInput").value = "";
-
 
 }
 
@@ -179,11 +182,11 @@ function resetTimer() {
     document.getElementById("WorksecondsInput").value = "";
 
 }
-function presetTimer(workMin, workSec, breakMin, breakSec){
+function presetTimer(workMin, workSec, breakMin, breakSec) {
     clearInterval(timer)
     document.getElementById("WorkminutesInput").value = workMin;
     document.getElementById("WorksecondsInput").value = workSec;
     document.getElementById("BreakminutesInput").value = breakMin;
     document.getElementById("BreaksecondsInput").value = breakSec;
-    
+
 }
